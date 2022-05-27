dd08df84dabc2a11a7919775083705abd75892cf
Yash Rathi <yashrathicricket@gmail.com>
2022-05-27 10:40:56 +0530

    Update test

--- tests/test_vimeo_downloader.py
+++ tests/test_vimeo_downloader.py
@@ -12,15 +12,33 @@ TEST_DATA = [
     {
         "url": "https://vimeo.com/503166067",
         "title": "We Don't Have To Know - Keli Holiday",
+        "id": "503166067",
     },
     {
         "url": "https://player.vimeo.com/video/498617513",
         "title": "NEET ekalavya all",
         "embedded_on": "https://atpstar.com/plans-162.html",
+        "id": "498617513",
     },
     {
         "url": "https://player.vimeo.com/video/98044508?some_random_p=parameter",
         "title": "Pier Solar OUYA Official Trailer",
+        "id": "98044508",
+    },
+    {
+        "url": "https://vimeo.com/392479337/a52724358e",
+        "title": "AMBIENT 1080 V4 ConstantQuality30",
+        "id": "392479337",
+    },
+    {
+        "url": "https://vimeo.com/160743502/abd0e13fb4",
+        "title": "Harrisville New Hampshire",
+        "id": "160743502",
+    },
+    {
+        "url": "https://vimeo.com/channels/staffpicks/143603739",
+        "id": "143603739",
+        "check_only_id": True,
     },
 ]
 
@@ -48,7 +66,16 @@ def _check_all_streams_work(streams: List[_Stream], video_title: str):
 
 
 @pytest.mark.parametrize("video_information", TEST_DATA)
+def test_extracted_id(video_information):
+    v = Vimeo(video_information["url"])
+    assert v._video_id == video_information["id"]
+
+
+@pytest.mark.parametrize("video_information", TEST_DATA)
 def test_download_video(video_information):
+    if video_information.get("check_only_id") is True:
+        return
+
     kwargs_for_vimeo_obj = {"url": video_information["url"]}
     if video_information.get("embedded_on"):
         kwargs_for_vimeo_obj["embedded_on"] = video_information["embedded_on"]
